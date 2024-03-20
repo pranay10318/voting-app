e0c6a1f80e2ca27d5e446c3a7cd41caa0087b3c6
Pranay <pradharapu@demandbase.com>
2024-03-20 20:20:59 +0530

    final commit level-4

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    experimentalStudio: true,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    experimentalStudio: true
   },
 });
